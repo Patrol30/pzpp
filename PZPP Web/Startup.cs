ab24803e26adea438b0e05753fd35ac5f967fa23
Patrol30 <patrol302@wp.pl>
2018-04-04 20:29:36 +0200

    pusty projekt

---
+++ PZPP Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PZPP_Web.Startup))]
+namespace PZPP_Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
