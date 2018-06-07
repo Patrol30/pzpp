c24c48e0e43948549998cd0edc3966d5c89849b9
Patrol30 <patrol302@wp.pl>
2018-06-07 03:38:37 +0200

    Zostało jeszcze samo dodawanie do roli reszta jest i działa

--- PZPP Web/Controllers/RolesController.cs
+++ PZPP Web/Controllers/RolesController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PZPP_Web.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,10 +10,35 @@ namespace PZPP_Web.Controllers
     [Authorize(Roles ="Admin")]
     public class RolesController : Controller
     {
+        string roleId;
         // GET: Roles
         public ActionResult Index()
         {
-            return View();
+            var model = new ApplicationDbContext().Roles.ToList();
+            return View(model);
+        }
+        public ActionResult UserList(string id)
+        {
+            var role = new ApplicationDbContext().Roles.Where(x => x.Id == id).First();
+            roleId = id;
+            List<PZPP_Web.Models.ApplicationUser> model = new ApplicationDbContext().Users.Where(x=>x.Roles.Where(y=>y.RoleId.Contains(id)).Any()).ToList();            
+            return View(model);
+        }
+        public ActionResult AddUserToRole()
+        {
+            var model = new ApplicationDbContext().Users.ToList();
+            return View(model);
+        }
+        public ActionResult AddUserToRole(string id)
+        {
+            roleId = id;
+            var model = new ApplicationDbContext().Users.ToList();
+            return View(model);
+        }
+        public ActionResult Delete(string id)
+        {
+            System.Web.Security.Roles.RemoveUserFromRole(id, roleId);            
+            return RedirectToAction("UserList", roleId);
         }
     }
 }
